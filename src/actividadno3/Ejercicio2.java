1fa8f6c16f025ec7f5936675032c3f04ce22086e
DeSebasWeb <121350219+DeSebasWeb@users.noreply.github.com>
2024-06-03 15:35:01 -0500

    Actualizacion Ejercicio2

---
+++ src/actividadno3/Ejercicio2.java
@@ -0,0 +1,29 @@
+package actividadno3;
+/**2.Hacer un programa en Java que cuente y sume los múltiplos de 5 y 3
+ *(para ser sumado debe verificarse que sea múltiplo de 5 y 3 a la vez)
+ * comprendidos entre 1 y n (n es determinado por el usuario).
+ * Imprimir la cantidad de múltiplos de 5 y 3.
+ */ 
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        double suma = 0; 
+        int acumulador = 0;
+        double rangoSuma;
+        int cantMultiplos = -1;
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ponga un numero: ");
+        rangoSuma = leer.nextDouble();
+        int i = 0;
+        for(i=0; i<=rangoSuma; i++){
+            if(i%5==0 && i%3 ==0){
+                suma = suma+i;
+                cantMultiplos++;
+            }
+        }
+        System.out.println("El numero de multiplos de 5 y 3 es: "+ cantMultiplos);
+        System.out.println("La suma total de numeros multiplos de 3 y 5 es: "+ suma);
+    }
+}
