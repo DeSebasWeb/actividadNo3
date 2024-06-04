2905522086ac9c960d298f0c847af148f5260901
DeSebasWeb <121350219+DeSebasWeb@users.noreply.github.com>
2024-06-03 23:37:37 -0500

    Ejercicio 8: Realizar!!!!

---
+++ src/actividadno3/Ejercicio8.java
@@ -0,0 +1,13 @@
+package actividadno3;
+public class Ejercicio8 {
+    /**Dado N cantidad de precios de gaseosas en un supermercado 
+     * el gerente de ventas desea que se genere un programa que 
+     * le permita saber cuál de las gaseosas tiene el mayor precio, 
+     * cuál tiene menor precio y cuál es el precio promedio. 
+     * Hacer las impresiones según lo requerido
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
