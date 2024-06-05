3afe9499904c24d878fa22bf03631771ba1b6979
DeSebasWeb <121350219+DeSebasWeb@users.noreply.github.com>
2024-06-04 23:39:26 -0500

    Actualizacion ejercicio 12: por hacer

---
+++ src/actividadno3/Ejercicio12.java
@@ -0,0 +1,18 @@
+package actividadno3;
+
+public class Ejercicio12 {
+    /**Realizar un programa que genere una matriz 5x5, 
+    * inicializarla en 2 e imprimirla.
+    */
+    public static void main(String[] args) {
+        int matriz[][] = new int[5][5];
+        
+        for(int i = 0; i<5; i++){
+            for(int j = 0; j< 5; j++){
+                
+            }
+        }
+        
+    }   
+    
+}
